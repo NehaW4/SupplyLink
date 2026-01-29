55a75505d858c3ba7fcd3ef6cab989d4714ea88b
NehaW4 <dummy@gmail.com>
2026-01-29 18:00:51 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/entity/Product.java
@@ -0,0 +1,75 @@
+package com.edutech.progressive.entity;
+
+public class Product {
+
+    private int productId;
+    private int warehouseId;
+    private String productName;
+    private String productDescription;
+    private int quantity;
+    private long price;
+    
+    public Product() {
+    }
+
+    public Product(int productId, int warehouseId, String productName, String productDescription, int quantity,
+            long price) {
+        this.productId = productId;
+        this.warehouseId = warehouseId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(int warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    
+
+}
