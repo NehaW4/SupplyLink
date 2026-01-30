00a0db260b328a2833b31a8b6073914750a51049
NehaW4 <dummy@gmail.com>
2026-01-30 15:50:00 +0000

    day 2 Task

--- server/src/main/java/com/edutech/progressive/SupplyLinkApplication.java
+++ server/src/main/java/com/edutech/progressive/SupplyLinkApplication.java
@@ -3,5 +3,6 @@ package com.edutech.progressive;
 public class SupplyLinkApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to SupplyLink Project!");
+        System.out.println("day 2");
     }
 }
